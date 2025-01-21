2d1dc38f4ebcb9951d0862c9b2a5e432d333b3b5
kelio <keliofilho12@gmail.com>
2025-01-21 14:30:53 -0300

    Project created

---
+++ src/test/java/com/resthub/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resthub.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
