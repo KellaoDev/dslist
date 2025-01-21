ca1d8e77d5d5415ac3078fc505d4c1b89b6ea0e9
kelio <keliofilho12@gmail.com>
2025-01-21 17:27:06 -0300

    GET lists

---
+++ src/main/java/com/resthub/dslist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.resthub.dslist.dto;
+
+import com.resthub.dslist.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/resthub/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.resthub.dslist.services;
+
+import com.resthub.dslist.dto.GameListDTO;
+import com.resthub.dslist.entities.GameList;
+import com.resthub.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+}
