a216e2ae2bf9ad925541c7d7a4e89976c315ee6a
kelio <keliofilho12@gmail.com>
2025-01-23 15:05:04 -0300

    Move games on list

---
+++ src/main/java/com/resthub/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,16 @@
+package com.resthub.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+
+}
